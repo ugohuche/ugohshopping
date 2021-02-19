48a1da7710fbee6b5f4c6a6237543f6396dc8487
ugohuche <ucugooh@gmail.com>
2021-02-19 06:24:14 +0100

    Last commit

--- core/models.py
+++ core/models.py
@@ -42,7 +42,7 @@ class Item(models.Model):
   label = models.CharField(choices=LABEL_CHOICES, max_length=2)
   slug = models.SlugField()
   description = models.TextField()
-  image = models.ImageField()
+  image = models.CharField()
 
   objects = ItemQuerySet.as_manager()
 
